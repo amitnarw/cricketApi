99f8249d5ea06798b99858f075fc4518ffab0c90
amitnarw <71306477+amitnarw@users.noreply.github.com>
2022-04-26 21:40:10 +0530

    Update cri.sh

--- examples/bash/cri.sh
+++ examples/bash/cri.sh
@@ -2,6 +2,6 @@
 
 # cURL Request
 curl -s --request GET \
-    --url "https://cricket-api.vercel.app/live.php" \ | jq '.livescore.title, .livescore.current'
+    --url "https://cricket-api-amitnarw.vercel.app/live.php" \ | jq '.livescore.title, .livescore.current'
 
 exit 0
